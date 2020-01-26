ec51fe07a3daf55547037e66d1584becd43fee31
Christian Boesen <christian.sisseck@gmail.com>
2020-01-26 15:49:11 +0100

    init

---
+++ src/components/Skabelon.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="tagFoto">
+    <p>
+      sass styling
+    </p>
+      
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'tagFoto',
+  data () {
+    return {
+
+     
+    }
+  }, 
+  methods:{
+    tagEtFoto(){
+      console.log("Tag et foto");
+    }
+  }, 
+  created(){
+
+  }
+}
+</script>
+
+<style lang="scss"> 
+
+@import "./src/styles/main.scss";
+
+.tagFoto
+  {
+@include flexCenter();
+p{
+  background: $primaryBtn;
+}
+  }
+
+
+
+</style>
