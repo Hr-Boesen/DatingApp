04a687c48527b2077b366dec0615518a554a355f
koreshkov <koreshkov.andrei@gmail.com>
2020-03-30 23:01:05 +0200

    firebase auth

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,29 +1,41 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue/dist/vue.js'
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import './db';
+
 import App from './App'
 import router from './router'
 import store from './store'
 import BaseView from './components/common/BaseView'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
 // Create global filter
 Vue.filter('snippet', val => {
-    if(!val || typeof(val) != 'string'){
-        return ''
-    }else if(val = val.slice(0,32)){
-        return val
+    if (!val || typeof(val) !== 'string'){
+        return '';
+    } else if (val = val.slice(0,32)){
+        return val;
     }
 })
 
 Vue.component('BaseView', BaseView);
 
-/* eslint-disable no-new */
-new Vue({
-    el: '#app',
-    router,
-    store,
-    components: { App },
-    template: '<App/>'
+firebase.auth();
+
+let app;
+firebase.auth().onAuthStateChanged((user) => {
+    if (!app) {
+        app = new Vue({
+            el: '#app',
+            router,
+            store,
+            components: { App },
+            template: '<App/>'
+        })
+    }
+    store.commit('setUser', user);
 })
